70d3c2257a48669c0c679f47232a3f7c9488bb0e
Sam M <smil143@lsu.edu>
2017-01-22 14:53:48 -0600

    Version .8

--- Prism_Break/Assets/Scripts/PlayerHealth.cs
+++ Prism_Break/Assets/Scripts/PlayerHealth.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {	
-<<<<<<< HEAD
+
 	public float health = 5f;					// The player's health.
-=======
-	public float health = 5;					// The player's health.
->>>>>>> 25ac09ba62608a65abeb6cc6204e45219d355d9a
+
+	//public float health = 5;					// The player's health.
+ 
 	public float repeatDamagePeriod = 2f;		// How frequently the player can be damaged.
 	public AudioClip[] ouchClips;				// Array of clips to play when the player is damaged.
 	public float hurtForce = 400f;				// The force with which the player is pushed when hurt.
@@ -25,7 +25,7 @@ public class PlayerHealth : MonoBehaviour
 		playerControl = GetComponent<PlayerControl>();
 		//healthBar = GameObject.Find("HealthBar").GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
-		UpdateHealthBar ();
+		//UpdateHealthBar ();
 	}
 
 
@@ -69,7 +69,7 @@ public class PlayerHealth : MonoBehaviour
 		health -= damageAmount;
 		print("health: " + health);
 		// Update what the health bar looks like.
-		UpdateHealthBar();
+		//UpdateHealthBar();
 
 		// Play a random clip of the player getting hurt.
 		int i = Random.Range (0, ouchClips.Length);
@@ -77,33 +77,65 @@ public class PlayerHealth : MonoBehaviour
 	}
 
 
-	public void UpdateHealthBar ()
+	public void Update ()
 	{
-		int intHealth = (int) health;
+		GameObject health1 = GameObject.FindGameObjectWithTag ("health1");
+		Destroy (health1);
+		/*
+		if (health < 1)
+			Application.LoadLevel ("sceneKill");
+		else if (health < 2) {
+			GameObject health0 = GameObject.FindGameObjectWithTag ("health0");
+			Destroy (health0);
+		} else if (health < 3) {
+			GameObject health1 = GameObject.FindGameObjectWithTag ("health1");
+			Destroy (health1);
+		} else if (health < 4) {
+			GameObject health2 = GameObject.FindGameObjectWithTag ("health2");
+			Destroy (health2);
+		} else if (health < 5) {
+			GameObject health3 = GameObject.FindGameObjectWithTag ("health3");
+			Destroy (health3);
+		}
+		*/
+	}
 
+		/*
+		int intHealth = (int) health;
 		switch (intHealth)
 		{
 		case 0:
 			Application.LoadLevel ("sceneKill");
 			break;
 		case 1:
-			Debug.Log (intHealth);
+			GameObject health0 = GameObject.FindGameObjectWithTag ("health0");
+			Destroy (health0);
 			break;
 		case 2:
+			GameObject health1 = GameObject.FindGameObjectWithTag ("health1");
 			Debug.Log (intHealth);
+			Destroy (health1);
 			break;
 		case 3:
+			GameObject health2 = GameObject.FindGameObjectWithTag ("health2");
 			Debug.Log (intHealth);
+			Destroy (health2);
 			break;
 		case 4:
+			GameObject health3 = GameObject.FindGameObjectWithTag ("health3");
 			Debug.Log (intHealth);
+			Destroy (health3);
 			break;
 		case 5:
+			GameObject health4 = GameObject.FindGameObjectWithTag ("health4");
 			Debug.Log (intHealth);
+			Destroy (health4);
 			break;
 		default:
 			Debug.Log (intHealth);
 			break;
 		}
+
 	}
+*/
 }
