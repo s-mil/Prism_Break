9db4d11e8c0442adf1b456800afbbda957376417
Sam M <smil143@lsu.edu>
2017-01-22 15:42:54 -0600

    Version 1.01

---
+++ Prism_Break/Assets/Scripts/Win.cs
@@ -0,0 +1,16 @@
+﻿
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class Win: MonoBehaviour {
+
+		public Texture backgroundTexture;
+
+		void OnGUI(){
+
+			//Display background texture
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
+		}
+}
