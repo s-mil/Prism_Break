f9211c1312dc7b44cc75d0ed9fb9589f6f49d731
ssicard <ssicar1@lsu.edu>
2017-01-22 01:41:41 -0600

    Kill Screen added

---
+++ Prism_Break/Assets/Scripts/Kill.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// Kill Scene
+/// attached to Main Camera
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	void OnGUI(){
+
+		//Display background texture
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
+	}
+
+	void Update(){
+		if (Input.GetButtonDown ("AButton")) {
+			//retry - go where
+			Application.LoadLevel ("world1");
+		}
+		if(Input.GetButtonDown("BButton")){
+			Application.Quit ();
+		}
+	}
+}
